f6b7b3855eb941e3271ac514739427b6a0f2c6c6
hexhowells <morganhowells314@gmail.com>
2026-08-23 20:28:58 +0100

    remove causal masking

--- tinyvit/model.py
+++ tinyvit/model.py
@@ -7,7 +7,7 @@ import math
 from tinyvit.utils import load_config
 
 
-class CausalSelfAttention(nn.Module):
+class SelfAttention(nn.Module):
     def __init__(self, config: dict):
         super().__init__()
         assert config['n_embd'] % config['n_head'] == 0
@@ -34,12 +34,12 @@ class CausalSelfAttention(nn.Module):
         q = q.view(B, T, self.n_head, C // self.n_head).transpose(1, 2) # (B, n_head, T, head_dim)
         v = v.view(B, T, self.n_head, C // self.n_head).transpose(1, 2) # (B, n_head, T, head_dim)
 
-        # causal self-attention; Self-attend: (B, n_head, T, head_dim) x (B, n_head, head_dim, T) -> (B, n_head, T, T)
+        # self-attention; Self-attend: (B, n_head, T, head_dim) x (B, n_head, head_dim, T) -> (B, n_head, T, T)
         y = F.scaled_dot_product_attention(
             q, k, v, 
             attn_mask=None, 
             dropout_p=self.attn_dropout.p if self.training else 0.0, 
-            is_causal=True
+            is_causal=False
         )
         y = y.transpose(1, 2).contiguous().view(B, T, C) # re-assemble all head outputs side by side
 
@@ -70,7 +70,7 @@ class Block(nn.Module):
     def __init__(self, config: dict):
         super().__init__()
         self.ln_1 = nn.LayerNorm(config['n_embd'])
-        self.attn = CausalSelfAttention(config)
+        self.attn = SelfAttention(config)
         self.ln_2 = nn.LayerNorm(config['n_embd'])
         self.mlp = MLP(config)
 
